1f97f1ecaa06010ce7eab4ae5e9d821d3eadc16e
Aaron Casanova <aaronccasanova@gmail.com>
2022-05-01 13:53:52 -0700

    feat: Add initial tsup recipe

---
+++ packages/tsup-ts/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  clean: true,
+  splitting: false,
+
+  // This custom tsconfig doesn't appear to be respected by tsup.
+  // Manually defining `target`, `sourcemap`, and `dts` compiler options.
+  tsconfig: './tsconfig.json',
+  target: ['es2020'],
+  sourcemap: true,
+  dts: true,
+})
