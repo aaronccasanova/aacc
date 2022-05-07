37da439391eb05fb6f51be1d62473d24ea5fceee
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-05-06 22:27:28 -0700

    Move from type:module to .mjs

--- packages/tsc-ts/.eslintrc.js
+++ packages/tsc-ts/.eslintrc.js
@@ -1,6 +1,11 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
   extends: ['@aacc/eslint-config/typescript'],
+  // files: ['*.ts', '*.tsx'],
   parserOptions: {
+    // tsconfigRootDir: __dirname,
     project: './tsconfig.json',
   },
 }
