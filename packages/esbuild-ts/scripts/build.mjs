863499829e7bb586e1d2d41162605da2c617cc1d
Aaron Casanova <aaronccasanova@gmail.com>
2022-05-30 23:34:56 -0700

    Initial esbuild recipe

---
+++ packages/esbuild-ts/scripts/build.mjs
@@ -0,0 +1,43 @@
+import esbuild from 'esbuild'
+
+// https://esbuild.github.io/api/#platform
+// https://esbuild.github.io/api/#format
+
+const common = {
+  entryPoints: ['src/index.ts'],
+  sourcemap: true,
+  target: [
+    'es2020',
+    'chrome58',
+    'edge16',
+    'firefox57',
+    'ie11',
+    'ios10',
+    'node14',
+    'opera45',
+    'safari11',
+  ],
+}
+
+await esbuild.build({
+  ...common,
+  platform: 'node',
+  format: 'cjs',
+  outdir: 'dist/cjs',
+})
+
+await esbuild.build({
+  ...common,
+  platform: 'neutral',
+  format: 'esm',
+  outdir: 'dist/esm',
+  outExtension: { '.js': '.mjs' },
+})
+
+await esbuild.build({
+  ...common,
+  platform: 'browser',
+  format: 'iife',
+  outdir: 'dist/browser',
+  bundle: true,
+})
---
+++ packages/esbuild-ts/src/index.ts
@@ -0,0 +1,6 @@
+import answer from 'the-answer'
+
+// eslint-disable-next-line no-console
+console.log(`the answer is ${String(answer)}!`)
+
+export * from './greet'
