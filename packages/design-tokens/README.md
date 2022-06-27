bc8f2f11c51c20447c93b00a49ad87c8c9b808b2
Aaron Casanova <aaronccasanova@gmail.com>
2022-06-26 22:47:37 -0700

    Use TypeScript compiler API to build  declaration files

--- packages/design-tokens/README.md
+++ packages/design-tokens/README.md
@@ -10,21 +10,48 @@ npm i @aacc/design-tokens
 
 ## Usage
 
-### CSS
+### CSS API
 
-#### Theme Variables
+#### Theme variables
 
 Include the `theme` styles directly if you bundler supports CSS imports:
 
 ```js
-import '@aacc/design-tokens/dist/css/theme-vars.css'
+import '@aacc/design-tokens/css/theme-vars.css'
 ```
 
-2. Otherwise, include the `theme` styles in your HTML:
+Otherwise, include the `theme` styles in your HTML:
 
 ```html
 <link
   rel="stylesheet"
   href="https://unpkg.com/@aacc/design-tokens@{version}/dist/css/theme-vars.css"
 />
+```
+
+### JavaScript API
+
+#### Theme object
+
+```js
+import { dark, dim, light } from '@aacc/design-tokens/themes'
+```
+
+```js
+const { dark, dim, light } = require('@aacc/design-tokens/themes')
+```
+
+### All the things
+
+```js
+import {
+  designTokens,
+  themes,
+  lightTheme,
+  darkTheme,
+  dimTheme,
+  colors,
+  spacing,
+  // etc...
+} from '@aacc/design-tokens'
 ```
--- packages/design-tokens/scripts/themesToThemeObjs.ts
+++ packages/design-tokens/scripts/themesToThemeObjs.ts
@@ -2,6 +2,9 @@ import * as fs from 'node:fs'
 import * as path from 'node:path'
 
 import type * as tf from 'type-fest'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as ts from 'typescript'
 import set from 'just-safe-set'
 import {
   traverse,
@@ -12,7 +15,12 @@ import {
 
 import type { DesignTokens, Themes } from '../src'
 
-const outputDir = path.join(__dirname, '../dist/theme')
+const outputDir = path.join(__dirname, '../dist/themes')
+const tsPaths: string[] = []
+
+const convertedCompilerOptions = getConvertedCompilerOptions(
+  path.join(__dirname, '../tsconfig.json'),
+)
 
 export async function themesToThemeObjs(themes: Themes) {
   if (!fs.existsSync(outputDir)) {
@@ -24,11 +32,64 @@ export async function themesToThemeObjs(themes: Themes) {
       const [themeKey, designTokens] = entry as tf.Entry<Themes>
 
       const theme = toTheme(designTokens)
+      const rawSource = `${themeKey} = ${JSON.stringify(theme, null, 2)}`
 
-      await fs.promises.writeFile(
-        path.join(outputDir, `${themeKey}.ts`),
-        `export const ${themeKey} = ${JSON.stringify(theme, null, 2)} as const`,
-      )
+      const cjsThemePath = path.join(outputDir, `${themeKey}.js`)
+      const esmThemePath = path.join(outputDir, `${themeKey}.mjs`)
+      const tsThemePath = path.join(outputDir, `${themeKey}.ts`)
+
+      tsPaths.push(tsThemePath)
+
+      await Promise.all([
+        fs.promises.writeFile(cjsThemePath, `exports.${rawSource}`),
+        fs.promises.writeFile(esmThemePath, `export const ${rawSource}`),
+        fs.promises.writeFile(
+          tsThemePath,
+          `export const ${rawSource} as const`,
+        ),
+      ])
+    }),
+  )
+
+  const cjsIndexPath = path.join(outputDir, 'index.js')
+  const esmIndexPath = path.join(outputDir, 'index.mjs')
+  const tsIndexPath = path.join(outputDir, 'index.ts')
+
+  await Promise.all([
+    await fs.promises.writeFile(
+      cjsIndexPath,
+      tsPaths
+        .map((themePath) => {
+          const themeKey = path.parse(themePath).name
+
+          return `exports.${themeKey} = require('./${themeKey}')`
+        })
+        .join('\n'),
+    ),
+    await fs.promises.writeFile(
+      esmIndexPath,
+      tsPaths
+        .map((themePath) => `export * from './${path.parse(themePath).name}'`)
+        .join('\n'),
+    ),
+    await fs.promises.writeFile(
+      tsIndexPath,
+      tsPaths
+        .map((themePath) => `export * from './${path.parse(themePath).name}'`)
+        .join('\n'),
+    ),
+  ])
+
+  compile([tsIndexPath], {
+    ...convertedCompilerOptions,
+    declarationDir: outputDir,
+    emitDeclarationOnly: true,
+  })
+
+  // Remove all tsPaths (only used to emit declarations)
+  await Promise.all(
+    [tsIndexPath, ...tsPaths].map(async (tsPath) => {
+      await fs.promises.rm(tsPath)
     }),
   )
 }
@@ -79,4 +140,91 @@ function toTheme(designTokens: DesignTokens) {
   })
 
   return theme
+}
+
+/**
+ * Minimal compiler:
+ * https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API#a-minimal-compiler
+ */
+function compile(fileNames: string[], options: ts.CompilerOptions): void {
+  const program = ts.createProgram(fileNames, options)
+  const emitResult = program.emit()
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics)
+
+  allDiagnostics.forEach((diagnostic) => {
+    if (diagnostic.file) {
+      const { line, character } = ts.getLineAndCharacterOfPosition(
+        diagnostic.file,
+        diagnostic.start!,
+      )
+
+      const message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        '\n',
+      )
+
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${
+          character + 1
+        }): ${message}`,
+      )
+    } else {
+      console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'))
+    }
+  })
+
+  const exitCode = emitResult.emitSkipped ? 1 : 0
+
+  if (exitCode !== 0) {
+    process.exit(exitCode)
+  }
+}
+
+function getConvertedCompilerOptions(filePath: string) {
+  const compilerOptions = ts.convertCompilerOptionsFromJson(
+    getCompilerOptionsJSONFollowExtends(filePath),
+    '',
+  )
+
+  if (compilerOptions.errors.length > 0) {
+    throw new Error(
+      `Failed to get converted compiler options: ${JSON.stringify(
+        compilerOptions.errors,
+        null,
+        2,
+      )}`,
+    )
+  }
+
+  return compilerOptions.options
+}
+
+/**
+ * How to compile tsconfig.json into a config object using TypeScript API?
+ * https://stackoverflow.com/a/68008175
+ */
+function getCompilerOptionsJSONFollowExtends(filePath: string) {
+  let compilerOptions = {}
+
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const config = ts.readConfigFile(filePath, ts.sys.readFile)
+    .config as tf.TsConfigJson
+
+  if (config.extends) {
+    const extendedConfigPath = require.resolve(
+      config.extends.startsWith('./') || config.extends.startsWith('../')
+        ? path.join(path.dirname(filePath), config.extends)
+        : config.extends,
+    )
+
+    compilerOptions = getCompilerOptionsJSONFollowExtends(extendedConfigPath)
+  }
+
+  return {
+    ...compilerOptions,
+    ...config.compilerOptions,
+  }
 }
