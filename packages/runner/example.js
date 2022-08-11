756fdbb30919bee0fca252665afbdaa4cb073233
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-08-11 08:35:20 -0700

    Update example.js

--- packages/runner/example.js
+++ packages/runner/example.js
@@ -9,18 +9,16 @@ const tally = {
 }
 
 async function main() {
-  const filez = await globby(['../../**/node_modules/**/*.js'], {
+  const files = await globby(['../../**/node_modules/**/*.js'], {
     absolute: true,
   })
-
-  const files = filez.slice(0, 2000)
 
   await runner({
     // Comment in/out the following line to test workers vs concurrent pending promises
     // withWorkers: false,
     //
     workers: 8,
-    files,
+    files: files.slice(0, 2000),
     processor: path.join(__dirname, './example-processor.js'),
     onProcessed: (result) => {
       tally.imports += result.imports
