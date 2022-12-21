dae5b58a1fb325d7f8f8899a495663b854e62d3a
Aaron Casanova <aaronccasanova@gmail.com>
2022-12-21 12:16:07 -0800

    NPM is protecting cross-runtime from being typosquatted

--- packages/crossruntime/examples/runtime.js
+++
@@ -1,7 +0,0 @@
-import * as cr from '../src/index.js'
-
-console.log('getRuntime:', cr.getRuntime())
-console.log('isDeno:', cr.isDeno())
-console.log('isNode:', cr.isNode())
-console.log('isBrowser:', cr.isBrowser())
-console.log('isBun:', cr.isBun())
--- packages/crossruntime/src/Augment.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'web-worker' {
-  declare const _default: typeof globalThis.Worker
-  export default _default
-}
