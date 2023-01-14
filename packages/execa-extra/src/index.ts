093a5ae98bc466682d34af2c0e4ceb85a3f4bc2d
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-01-14 13:34:30 -0800

    BREAKING:  improvements (#71)
    Co-authored-by: ehmicky <ehmicky@users.noreply.github.com>

--- packages/execa-extra/src/index.ts
+++ packages/execa-extra/src/index.ts
@@ -1,48 +1,92 @@
-import { command, Options, ExecaChildProcess } from 'execa'
+import execa, { Options, ExecaChildProcess, ExecaSyncReturnValue } from 'execa'
 
-type Expressions = (string | number)[]
+const SPACES_REGEXP = / +/g
 
-type ConfiguredExecaCommand = (
-  templates: TemplateStringsArray,
-  ...expressions: Expressions
-) => ReturnType<typeof runExecaCommand>
+type TemplatesOrOptions = TemplateStringsArray | Options<string | null>
+type Expression = string | number | Array<string | number>
 
-type ExecaCommandResult<T> = T extends TemplateStringsArray
-  ? ReturnType<typeof runExecaCommand>
-  : ConfiguredExecaCommand
+type $Result<T extends TemplatesOrOptions> = T extends TemplateStringsArray
+  ? ExecaChildProcess
+  : (
+      templates: TemplateStringsArray,
+      ...expressions: Expression[]
+    ) => T extends { encoding: null }
+      ? ExecaChildProcess<Buffer>
+      : ExecaChildProcess
 
-export function execaCommand<T extends Options | TemplateStringsArray>(
+type $SyncResult<T extends TemplatesOrOptions> = T extends TemplateStringsArray
+  ? ExecaSyncReturnValue
+  : (
+      templates: TemplateStringsArray,
+      ...expressions: Expression[]
+    ) => T extends { encoding: null }
+      ? ExecaSyncReturnValue<Buffer>
+      : ExecaSyncReturnValue
+
+export function $<T extends TemplatesOrOptions>(
   templatesOrOptions: T,
-  ...expressions: Expressions
-): ExecaCommandResult<T> {
+  ...expressions: Expression[]
+): $Result<T> {
   if (isTemplateStringsArray(templatesOrOptions)) {
-    return runExecaCommand(
+    const [file = '', ...args] = parseTemplates(templatesOrOptions, expressions)
+    return execa(file, args) as $Result<T>
+  }
+
+  return ((templates, ...innerExpressions) => {
+    const [file = '', ...args] = parseTemplates(templates, innerExpressions)
+    return execa(
+      file,
+      args,
+      // @ts-expect-error - Execa types do allow `null` for `encoding`
       templatesOrOptions,
-      expressions,
-    ) as ExecaCommandResult<T>
-  }
-
-  return createExecaCommand(templatesOrOptions) as ExecaCommandResult<T>
+    )
+  }) as $Result<T>
 }
 
-export function createExecaCommand(options: Options): ConfiguredExecaCommand {
-  return function configuredExecaCommand(templates, ...expressions) {
-    return runExecaCommand(templates, expressions, options)
+$.sync = <T extends TemplatesOrOptions>(
+  templatesOrOptions: T,
+  ...expressions: Expression[]
+): $SyncResult<T> => {
+  if (isTemplateStringsArray(templatesOrOptions)) {
+    const [file = '', ...args] = parseTemplates(templatesOrOptions, expressions)
+    return execa.sync(file, args) as $SyncResult<T>
   }
+
+  return ((templates, ...innerExpressions) => {
+    const [file = '', ...args] = parseTemplates(templates, innerExpressions)
+    return execa.sync(
+      file,
+      args,
+      // @ts-expect-error - Execa types do allow `null` for `encoding`
+      templatesOrOptions,
+    )
+  }) as $SyncResult<T>
 }
 
-function runExecaCommand(
+function parseTemplate(
+  template: string,
+  index: number,
   templates: TemplateStringsArray,
-  expressions: Expressions,
-  options?: Options,
-): ExecaChildProcess<string> {
-  return command(join(templates, expressions), options)
+  expressions: Expression[],
+) {
+  const templateString = template ?? templates.raw[index]
+  const templateTokens = templateString.split(SPACES_REGEXP).filter(Boolean)
+
+  if (index === expressions.length) return templateTokens
+
+  const expression = expressions[index]
+
+  return Array.isArray(expression)
+    ? [...templateTokens, ...expression.map(String)]
+    : [...templateTokens, String(expression)]
 }
 
-function join(templates: TemplateStringsArray, expressions: Expressions) {
-  return templates.reduce(
-    (acc, template, i) => `${acc}${template}${expressions[i] ?? ''}`,
-    '',
+function parseTemplates(
+  templates: TemplateStringsArray,
+  expressions: Expression[],
+) {
+  return templates.flatMap((template, index) =>
+    parseTemplate(template, index, templates, expressions),
   )
 }
 
